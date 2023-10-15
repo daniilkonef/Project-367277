fcdd7b3a58f8c497c7955ac5d6eca9fa8defd35a
DANIILKONEF <daniilkonef@yandex.ru>
2023-10-15 21:23:43 +0300

    Великолепно! Проект можно считать законченным и можно сдать на проверку, тк все функции реализованы успешно!

--- start.py
+++ start.py
@@ -1,55 +1,5 @@
-import os
-
-from core import find_record_in
-from core import delete_existing_contact_from
-from core import change_existing_contact_from
-
 from datamodel_pack.database_class import CreateDatabaseMachine
 from presenter_pack.presenter_class import CreatePresenterObject
-
-
-# file_database: str = "database.txt"
-#
-#
-# def show_all_contacts():
-#     memory = list()
-#     with (open(file_database, 'r', encoding="utf-8")) as db:
-#         for line in db:
-#             data = line.replace(";", " ")
-#             memory.append(line.strip())
-#             print(data, end="")
-
-
-# def get_size_of_database():
-#     memory = list()
-#     with (open(file_database, 'r', encoding="utf-8")) as db:
-#         for line in db:
-#             memory.append(line.strip())
-#     # print()
-#     # print(len(memory))
-#     return len(memory)
-
-
-# def get_virtual_database() -> list:
-#     memory = list()
-#     with (open(file_database, 'r', encoding="utf-8")) as db:
-#         for line in db:
-#             memory.append(line.strip())
-#     return memory
-
-
-# def add_new_contact():
-#     id_number = get_size_of_database() + 1
-#
-#     firstname = input("Введите Имя: ") or " - "
-#     last_name = input("Введите Фамилию: ") or " - "
-#     patronymic = input("Введите Отчество: ") or " - "
-#     phone_number = input("Введите номер телефона: ") or " - "
-#     splitter = str(";")
-#     new_record = str(
-#         id_number) + splitter + firstname.strip() + splitter + last_name.strip() + splitter + patronymic.strip() + splitter + phone_number.strip()
-#     with open(file_database, "a", encoding="utf-8") as db:
-#         db.write(new_record + "\n")
 
 
 def main():
@@ -72,8 +22,6 @@ def main():
               "5 - Удалить заметку по номеру ID; \n"
               "6 - Изменить заметку по номеру ID; \n\n"
               ">> ", end=" ")
-
-
 
         user_selected_is = int(input())
         print()
